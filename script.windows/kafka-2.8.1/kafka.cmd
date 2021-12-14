f96b8940a693d16a2669b170e3c293ab32a15cc5
Jean-Claude Parent <noursicain@gmail.com>
2021-12-14 10:06:40 -0500

    scan de kafka  2.8.1

---
+++ script.windows/kafka-2.8.1/kafka.cmd
@@ -0,0 +1,3 @@
+@echo off
+call D:\Vulnerabilite\Logiciels\dependency-check\bin\dependency-check.bat -scan  D:\Vulnerabilite\cibles\kafka_2.12-2.8.1  -out D:\DevCenter\Sources\Vulscan\script.windows\kafka-2.8.1
+pause
